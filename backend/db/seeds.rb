16fb057b8b318c84bd42c010e265694e69eddfe8
superonicw <superonicw@gmail.com>
2020-11-16 22:24:59 -0500

    Updated seed messages

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -2,20 +2,25 @@ require 'net/http'
 
 Trip.destroy_all
 
+puts "--- Downloading trips ---"
 response = Net::HTTP.get_response('arrivalist-puzzles.s3.amazonaws.com', '/national_travel.json')
 
 if response.is_a?(Net::HTTPSuccess)
   trips = JSON.parse(response.body)['data']
 
-  trips.each do |params|
+  puts "--- Import started (total #{trips.length} trips)---"
+
+  trips.each_with_index do |params, index|
     trip = {
       'trip_date' => Date.parse(params['trip_date']),
       'home_state' => params['home_state'],
       'trip_count' => params['trip_count'],
     }
 
+    puts "#{index+1} trips" if (index + 1) % 1000 == 0
+
     Trip.create(trip)
   end
 
-  puts "Imported #{Trip.count} trips"
+  puts "--- Imported #{Trip.count} trips ---"
 end
